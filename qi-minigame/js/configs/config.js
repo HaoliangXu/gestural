e3f5156bed3723f10e1b0583abf03604ab8b93d2
starcloudes <starcloudes@gmail.com>
2018-04-23 22:59:01 +0800

    ✅配置开发环境文件

---
+++ qi-minigame/js/configs/config.js
@@ -0,0 +1,16 @@
+/**
+ * 开发环境配置——开发模式
+ */
+
+export const GAME_DEV = true
+
+// 请求接口
+export const API_PORT = 'test.com'
+
+// 获取用户 openid 的接口
+export const OPENID_PATH = '/openid'
+
+// 建立 Websocket连接 的接口
+export const OPENID_PATH = '/ws'
+
+// 获取 update资源更新 的接口
