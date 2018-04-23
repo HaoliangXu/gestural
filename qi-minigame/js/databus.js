6bcc04f6bd189ec6a302ab41a9893aeb53f36fd7
starcloudes <starcloudes@gmail.com>
2018-04-23 23:03:18 +0800

    ✅新增全局状态管理

---
+++ qi-minigame/js/databus.js
@@ -0,0 +1,48 @@
+let instance
+
+/**
+ * 全局状态管理，回收
+ */
+export default class DataBus {
+  constructor() {
+    // 如果创建过DataBus了，就返回之前创造的
+    if (instance)
+      return instance
+
+    instance = this
+
+    this.init()
+
+  }
+
+  init() {
+    this.winNum = 0
+
+
+    /**
+     * 用户数据存的对象
+     * openid要进行初始化，这样子做的好处是用户以后再打开游戏的时候，openid会一直存在
+     */
+    this.userData = { openid: wx.getStorageSync('openid') || void 0 }
+
+    wx.getSystemInfo({
+      success: (res) => {
+        this.systemInfo = res
+      }
+    })
+    /**
+     * 游戏状态
+     * 改变状态是直接改变这个字符串，会自动检测并调用相应的渲染函数
+     * @type {String}
+     */
+    this.gameStatus = 'playing'
+
+    this.userInfo = {}
+   
+
+    this.gameControl = {
+      isNeedRefreshPlaying: true
+    }
+
+  }
+}
