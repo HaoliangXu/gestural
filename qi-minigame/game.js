b7c3c8c46f4f053c1b427ccdf5041be2770577a3
starcloudes <starcloudes@gmail.com>
2018-04-23 22:10:35 +0800

    ✅使用socketIO模拟前后端通信

--- qi-minigame/game.js
+++ qi-minigame/game.js
@@ -3,37 +3,6 @@ import './js/libs/symbol'
 
 import Main from './js/main'
 
-
-// 从服务器下载资源更新
-update()
-
-
-
-// 微信授权登陆
-function getUserInfo() {
-    Wechat.getCryptoData()
-      .then(d => {
-        return Wechat.getMyOpenid(d);
-      })
-      .then(d => {
-        console.log("从后端获取的openid", d.data);
-      })
-      .catch(e => {
-        console.log(e);
-      })
-  }
-  
-  
-  getUserInfo()
-
-
-
-// 初始化游戏首页
-render()
-
-
-
-// 点击开始游戏
 new Main()
 
 
---
+++ qi-minigame/js/test/socket.js
@@ -0,0 +1,55 @@
+const io = require('./libs/weapp.socket.io.js')
+const socket = io('http://localhost:3000')
+
+export default class Socket{
+
+  constructor() {
+    this.socket()
+  }
+
+
+  //连接websocket
+  socket() {
+
+    socket.on('hi', data => {
+      console.log('received msg: ', data)
+    })
+
+    socket.emit('hello', {
+      title: 'hello'
+    })
+
+    socket.emit('join', {
+      userInfo: 'nickName'
+    })
+
+    socket.on('start', data => {
+      console.log('received msg: ', data)
+    })
+
+    socket.emit('choice', {
+      userChoice: 'jiaqi'
+    })
+
+    socket.on('eachRound', data => {
+      console.log('received msg: ', data)
+    })
+
+    socket.emit('game', {
+      user: 'result'
+    })
+
+    socket.on('result', data => {
+      console.log('received msg: ', data)
+    })
+
+
+
+
+
+
+   
+  }
+
+
+}
